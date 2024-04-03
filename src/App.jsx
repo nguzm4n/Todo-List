4e7ce35fdca332e2c3b16faed80e173d5238f19d
Nicolás Guzmán <n3kguzman@gmail.com>
2024-04-03 17:25:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {React, useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='container contenedor-tareas'>
+
+    </div>
+    </>
+  )
+}
+
+export default App
